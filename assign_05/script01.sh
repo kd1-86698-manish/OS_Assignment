3d5acd1200330190f8f354a30323634f622a7970
manishk1199 <manishkarne1967@gmail.com>
2024-10-12 20:19:31 +0530

    .

---
+++ assign_05/script01.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+#-------------------------------------------------------------------------
+#Aim : Write a shell script to display your LOGIN NAME and HOME directory.
+#-------------------------------------------------------------------------
+
+echo  "Login Name :: $USER"
+echo  "Home Directory  :: $HOME"
